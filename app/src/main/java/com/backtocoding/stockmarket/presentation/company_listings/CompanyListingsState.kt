2b3109781d7393b14f90cf6b077177c3cb73fcc9
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-06-29 20:56:47 +0530

    Added: CompanyListings Viewmodel

---
+++ app/src/main/java/com/backtocoding/stockmarket/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,11 @@
+package com.backtocoding.stockmarket.presentation.company_listings
+
+import com.backtocoding.stockmarket.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
+
